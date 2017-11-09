9c77d18821ff2881267487f62549a6f7bccfe739
Patrick Carroll <patrickgesarcarroll@gmail.com>
2017-11-09 11:41:55 -0800

    typeof: Feature
    The default prompt at the bottom of the screen says Name? Instead of
    Send. When you type your first message, it will save whatever you have
    put down as name into a variable. Functionality for testing is sending
    messages to the commandline
    
    Summary: Username input captured now

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', function(req, res) {
 io.on('connection', function(socket){
 	console.log('a user connected');
 	socket.on('chat message', function(msg) {
-		console.log('message: ' + msg);
+		console.log(msg);
 	})
 	socket.on('disconnect', function(socket){
 		console.log('a user disconnected');
