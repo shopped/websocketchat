d86ce104b249cd845e3a132df9e85e67fdf5992e
Patrick Carroll <patrickgesarcarroll@gmail.com>
2017-11-24 09:12:31 -0800

    bugfix
    line 23 of chatroom.js appending messages to chat

--- index.js
+++ index.js
@@ -18,13 +18,15 @@ var users = [];
 
 function onConnect(socket) {
 	var userName = "Unnamed"
-	socket.on('chat message', function(msg) {
+	socket.on('message', function(msg) {
 		io.emit('done typing', userName);
-		socket.broadcast.emit('chat message', msg);
+		socket.broadcast.emit('message', msg);
+	})
+	socket.on('bulletin', function(msg) {
+		socket.broadcast.emit('bulletin', msg);
 	})
 	socket.on('username given', function(username) {
 		userName = username;
-
 		users.push(userName);
 		socket.emit('populate', users);
 		socket.broadcast.emit('populate', users);
@@ -32,7 +34,7 @@ function onConnect(socket) {
 	socket.on('disconnect', function(socket){
 		if (userName === "Unnamed")
 			return
-		io.emit('chat message', userName + ' has disconnected.');
+		io.emit('bulletin', userName + ' has disconnected.');
 		users.splice(users.indexOf(userName), 1);
 		io.emit('populate', users);
 	})
--- static/chatroom.js
+++ static/chatroom.js
@@ -1,23 +1,39 @@
+function getColor() {
+  var letters = '0123456789AB'
+  var color = '#'
+  for (var i=0; i<6; i++){
+    color += letters[Math.floor(Math.random() * 12)]
+  }
+  return color;
+}
+
 $(function () {
 			var socket = io();
       var chatName = null;
+      var color = getColor();
       $('form').submit(function() {
         if (!chatName) {
           $('#b').html("Send");
           chatName = $('#m').val();
           socket.emit('username given', chatName)
-          socket.emit('chat message', chatName + ' has connected.')
+          socket.emit('bulletin', chatName + ' has connected.')
         } else {
-          msg = (chatName +': '+ $('#m').val());
-          $('#messages').append($('<li>').text(msg));
-          socket.emit('chat message', msg);
+          usr = $('<span>').text(chatName + ': ');
+          msg = $('<span>').text($('#m').val());
+          $('#messages').append($('<li>').append(usr).append(msg));
+          socket.emit('message', msg);
         }
         $('#m').val('');
         return false;
       });
-			socket.on('chat message', function(msg){
+			socket.on('message', function(msg){
 				$('#messages').append($('<li>').text(msg));
 			});
+      socket.on('bulletin', function(msg){
+        let b = $('<li>').text(msg);
+        b.css('color', 'gray');
+        $('#messages').append(b);
+      })
       socket.on('started typing', function(user){
         if ($('#'+user).length === 0) {
           var x = $('<li>').text(user + ' is typing...');
